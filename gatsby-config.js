bbfaff11a89f7c4420ddd2d3a0c0190902b65ff6
Paruyr Mamikonyan <paruyr@mamikonyan.info>
2019-07-07 17:20:47 +0400

    Setup: create blog starting files, images, data, etc...

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,58 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "mam1konyan blog",
+    description: "Բլոգ ծրագրավորման մասին և ոչ միայն",
+    author: "Paruyr Mamikonyan",
+    image: "/home.jpg",
+    siteUrl: "http://blog.mamikonyan.io",
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /\.inline\.svg$/,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-transition-link`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    `gatsby-plugin-playground`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://blog.mamikonyan.io",
+        sitemap: "https://blog.mamikonyan.io/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,37 @@
 import React from "react"
+import Layout from "../components/layout"
+import "../styles/styles.scss"
+import "./styles.scss"
+import { Typing } from "react-typing"
+import Content from "../components/content"
+import SEO from "../components/SEO"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  return (
+    <>
+      <Layout isArrowShown={null}>
+        <SEO title="Գլխավոր" description="Բլոգ ծրագրավորման մասին և ոչ միայն" author="Paruyr Mamikonyan" />
+        <section className="home__wrapper">
+          <div className="home__image-wrapper">
+            <div className="home__body">
+              <p className="home__text">
+                <Typing delay={25} keyDelay={30}>
+                  Ողջույն։ Ես Պարույրն եմ։
+                  <br />
+                  Չեմ խոստանում, բայց ժամանակ առ ժամանակ ես կփորձեմ կիսվել
+                  հետաքրքիր թեմաներով ծրագրավորման մասին (և ոչ միայն) այս
+                  բլոգում։
+                  <br />
+                  Բարի ժամանց!
+                </Typing>
+              </p>
+            </div>
+          </div>
+          <div className="home__content">
+            <Content />
+          </div>
+        </section>
+      </Layout>
+    </>
+  )
+}
